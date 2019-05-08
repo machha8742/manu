0eb2d61edb5af338cfdc134cc02f99d46a4d8466
machha <machha@gmail.com>
2019-05-08 11:31:03 +0530

    removed web and chrome

--- lambda-java-example/src/main/java/doc_examples/lambda_java_example/App.java
+++ lambda-java-example/src/main/java/doc_examples/lambda_java_example/App.java
@@ -1,7 +1,5 @@
 package doc_examples.lambda_java_example;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 /**
@@ -17,10 +15,6 @@ public class App
         
 
 
-    		System.setProperty("webdriver.chrome.driver","C:\\dev files\\drivers\\chromedriver.exe");
-    		WebDriver driver=new ChromeDriver();
-    		        driver.get("http://localhost:8090/spring3-mvc-maven-xml-hello-world-1.0-SNAPSHOT/");
-
-    		    }
+    }	
 
     }
